642e24bd5b74e7aedb0e1cd3f253794db2852cf5
Cristian Cettolo <cristian.cettolo@zeranta.com>
2025-03-14 15:27:39 +0100

    fix

--- app/events/matilda_core/users/edit_info_event.rb
+++ app/events/matilda_core/users/edit_info_event.rb
@@ -12,12 +12,15 @@ module MatildaCore
       payload_attributes_are :user_uuid, :name, :surname, :mask_sensitive_data, :log_who
 
       to_write_event do
+        # Assicuriamoci che mask_sensitive_data sia un valore booleano appropriato
+        mask_value = [true, '1', 1, 'true'].include?(payload[:mask_sensitive_data])
+        
         set_not_saved unless save_event && MatildaCore::User.find_by(
           uuid: payload[:user_uuid]
         )&.update(
           name: payload[:name],
           surname: payload[:surname],
-          mask_sensitive_data: payload[:mask_sensitive_data]
+          mask_sensitive_data: mask_value
         )
       end
 
