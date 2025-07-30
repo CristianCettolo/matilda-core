718fd0498e97ed1c319e0613fe574da66a362a0f
CristianCettolo <cristian.cettolo@zeranta.com>
2025-07-30 11:23:02 +0200

    fix2

--- app/events/matilda_core/users/edit_info_event.rb
+++ app/events/matilda_core/users/edit_info_event.rb
@@ -9,7 +9,7 @@ module MatildaCore
 
       name_is :matilda_core_users_edit_info
 
-      payload_attributes_are :user_uuid, :name, :surname, :mask_sensitive_data, :log_who
+      payload_attributes_are :user_uuid, :name, :surname, :mask_sensitive_data, :log_who, :username, :units_system, :hide_useless_sessions
 
       to_write_event do
         set_not_saved unless save_event && MatildaCore::User.find_by(
