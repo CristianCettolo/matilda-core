983f599b10b91cc88e1c998300eab4db5add2e87
CristianCettolo <cristian.cettolo@zeranta.com>
2025-08-07 11:42:33 +0200

    update

--- lib/matilda_core/engine.rb
+++ lib/matilda_core/engine.rb
@@ -26,15 +26,6 @@ module MatildaCore
         permission: 'matilda_core.memberships',
         index: 1100
       )
-      if Rails.env.development?
-        MatildaCore.config.add_sidebar_item(
-          'matilda_core.documentation',
-          label: 'locale.matilda_core.titles.documentation',
-          url: MatildaCore::Engine.routes.url_helpers.documentation_index_view_path,
-          icon: 'fas fa-code',
-          index: 999999
-        )
-      end
     end
 
     # Imposto i permessi degli utenti.
